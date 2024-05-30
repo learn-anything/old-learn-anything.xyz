daeccb582a08b20d145249ce1e7bdb37f37b8d3b
marshennikovaolga <marshennikova@gmail.com>
2024-05-30 20:30:05 +0300

    .

--- components/routes/ProfileRoute.tsx
+++ components/routes/ProfileRoute.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { observer, useObservable } from "@legendapp/state/react"
+import { GoSearch } from "react-icons/go"
 
 interface Props {}
 export default observer(function ProfileRoute(props: Props) {
@@ -36,8 +37,37 @@ export default observer(function ProfileRoute(props: Props) {
 		],
 	})
 	return (
-		<>
-			<div className="text-white">profile</div>
-		</>
+		<div
+			className="p-4 text-white min-h-screen"
+			style={{ backgroundColor: "#0f0f0f" }}
+		>
+			<div className="space-y-4">
+				{local.personalLinks.map((link, index) => (
+					<div
+						key={index}
+						style={{ backgroundColor: "#121212" }}
+						className="flex items-center justify-between p-4 rounded"
+					>
+						<div className="flex items-center">
+							<a className="text-white hover:underline">
+								{link.title.toString()}
+							</a>
+						</div>
+						<span className="text-zinc-600">{link.topic.toString()}</span>
+					</div>
+				))}
+			</div>
+			<div className="flex justify-center mt-4">
+				<div className="relative w-[30%]">
+					<input
+						type="text"
+						placeholder="Search or Add"
+						style={{ backgroundColor: "#202020" }}
+						className="text-zinc-400 placeholder:text-zinc-700 pl-10 placeholder:text-center px-2 py-3 rounded-xl w-full border border-zinc-600"
+					/>
+					<GoSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-xl text-zinc-700" />
+				</div>
+			</div>
+		</div>
 	)
 })
