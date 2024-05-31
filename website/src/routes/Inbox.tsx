73ec6a1ba32ed4f579033bd21c7ca13da58923c1
Anselm <anselm.eickhoff@gmail.com>
2024-06-01 00:46:39 +0200

    Sketch out Inbox page

--- website/src/routes/Inbox.tsx
+++ website/src/routes/Inbox.tsx
@@ -1,3 +1,27 @@
+import { useAccount, useCoState } from "../main"
+import { PersonalLink, TodoItem } from "../schema";
+
 export default function InboxPage() {
-	return <>Inbox</>
+	const { me } = useAccount({ root: {inbox: [{}]}});
+
+	// const topic = useCoState(Topic, topicID)
+
+	return <><h1>Inbox</h1>{
+		me?.root?.inbox?.map((item) => {
+			item instanceof TodoItem ? (
+				<TodoItemComponent item={item} />
+			) : (
+				<PersonalLinkComponent item={item} />
+			)
+		})
+	}</>
+}
+
+
+export function TodoItemComponent({ item }: { item: TodoItem }) {
+	return ""
+}
+
+export function PersonalLinkComponent({ item }: { item: PersonalLink }) {
+	return ""
 }
