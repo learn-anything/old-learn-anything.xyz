ab516b18ea159ea3959b3d236c5a8281204bbec6
Anselm <anselm.eickhoff@gmail.com>
2024-06-01 00:34:04 +0200

    FIrst jazz schema draft

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+	return;
 }
 
 export default App
--- website/src/main.tsx
+++ website/src/main.tsx
@@ -2,9 +2,18 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
+import { createJazzReactContext, DemoAuth } from "jazz-react"
+
+const Jazz = createJazzReactContext({
+	auth: DemoAuth({ appName: "Learn Anything" }),
+	peer: "wss://mesh.jazz.tools/?key=nikita@nikiv.dev", // <- put your email here to get a proper API key later
+})
+export const { useAccount, useCoState } = Jazz
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<Jazz.Provider>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Jazz.Provider>,
 )
