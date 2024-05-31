0a01295904634e55df0f67a76464d64de05a9e2f
marshennikovaolga <marshennikova@gmail.com>
2024-05-31 13:16:16 +0300

    .

--- app/[profile]/page.tsx
+++ app/[profile]/page.tsx
@@ -13,7 +13,7 @@ export default function Profile() {
 				<div className="w-1/6">
 					<Sidebar personalPages={[]} />
 				</div>
-				<div className="flex-1 flex flex-col">
+				<div className="flex-1 flex flex-col border-2 border-neutral-900 rounded-lg m-4">
 					<Topbar showView={showView} />
 					<ProfileRoute />
 				</div>
