cf218e13895d1e379a24c4bc7b51028a971687b5
marshennikovaolga <marshennikova@gmail.com>
2024-06-19 18:42:28 +0400

    .

--- web/src/routes/SearchRoute.tsx
+++ web/src/routes/SearchRoute.tsx
@@ -1,13 +1,12 @@
 import Sidebar from "../components/Sidebar"
-import { icons } from "../components/Icons"
 
 export default function SearchRoute() {
 	return (
-		<div className="flex h-screen mt-2 mb-2 max-w-screen w-screen overflow-hidden text-white">
-			<div className="w-1/7">
+		<div className="flex h-screen w-screen box-border overflow-hidden text-white">
+			<div className="w-1/7 mr-2">
 				<Sidebar />
 			</div>
-			<div className="w-full mx-5 h-full border border-white/10 rounded-[20px]">
+			<div className="w-full mt-2 h-full border border-white/10 rounded-[20px] mr-2">
 				<div className="px-5 py-3">
 					<input className="bg-[#121212] placeholder:font-light tracking-wide font-light outline-none rounded-[10px] placeholder-white/20 text-white/30 w-[70%] px-5 py-3"></input>
 				</div>
