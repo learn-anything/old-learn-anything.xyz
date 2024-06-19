108ed3bb0665d495c76a4b13429acce0b78df3b1
marshennikovaolga <marshennikova@gmail.com>
2024-06-19 18:07:44 +0400

    .

--- web/src/routes/ProfileRoute.tsx
+++ web/src/routes/ProfileRoute.tsx
@@ -12,9 +12,9 @@ interface ProfileStatsProps {
 	label: string
 }
 
-interface ProfilePagesProps {
-	topic: string
-}
+// interface ProfilePagesProps {
+// 	topic: string
+// }
 
 interface ProfileLinksProps {
 	linklabel: string
@@ -31,15 +31,15 @@ const ProfileStats: React.FC<ProfileStatsProps> = ({ number, label }) => {
 	)
 }
 
-const ProfilePages: React.FC<ProfilePagesProps> = ({ topic }) => {
-	return (
-		<div className="rounded-lg text-white items-center p-3 cursor-pointer bg-[#121212] flex flex-row justify-between">
-			<p>{topic}</p>
+// const ProfilePages: React.FC<ProfilePagesProps> = ({ topic }) => {
+// 	return (
+// 		<div className="rounded-lg text-white items-center p-3 cursor-pointer bg-[#121212] flex flex-row justify-between">
+// 			<p>{topic}</p>
 
-			<icons.VectorArrowRight />
-		</div>
-	)
-}
+// 			<icons.VectorArrowRight />
+// 		</div>
+// 	)
+// }
 
 const ProfileLinks: React.FC<ProfileLinksProps> = ({
 	linklabel,
@@ -49,7 +49,7 @@ const ProfileLinks: React.FC<ProfileLinksProps> = ({
 	return (
 		<div className="rounded-lg text-white items-center p-3 bg-[#121212] flex flex-row justify-between">
 			<div className="flex flex-row space-x-3 items-center">
-				<p className="text-white/40 text-base">{linklabel}</p>
+				<p className="text-white/90 text-base">{linklabel}</p>
 				<div className="flex flex-row items-center gap-1 cursor-pointer">
 					<icons.Link />
 					<p className="text-white/10 text-sm">{link}</p>
@@ -92,80 +92,78 @@ const ProfileRoute = () => {
 	}
 
 	return (
-		<>
-			<div className="flex flex-col h-screen py-3">
-				<div className="flex flex-1">
-					<div className="w-1/5 mr-2">
-						<Sidebar />
-					</div>
-					<div className="flex-1 flex flex-col border border-neutral-800 rounded-3xl">
-						<p className="text-2xl text-white/30 font-semibold h-[74px] p-[20px]">
-							{params.topicOrUsername}
-						</p>
-						<div className="flex flex-col items-center bg-inherit text-white pb-5 border-b border-neutral-900">
-							<div className="flex align-top w-full max-w-2xl">
-								<div className="w-[130px] h-[130px] bg-[#222222] rounded-md mr-3" />
-								<div className="ml-6 flex-1">
-									<p className="text-[25px] font-semibold mb-3">
-										Username Username
-									</p>
-									<div className="text-white text-[24] font-light mb-1 flex flex-row">
-										<icons.Link />
-										<p className="pl-1">Usernametext</p>
-									</div>
-									<a
-										href="https://"
-										className="text-white/40 text-sm font-light mb-1 flex flex-row items-center"
-									>
-										<icons.Link />
-										<p className="pl-1">usernamelink</p>
-									</a>
+		<div className="flex flex-col h-screen py-3">
+			<div className="flex flex-1">
+				<div className="w-1/5 mr-2">
+					<Sidebar />
+				</div>
+				<div className="flex-1 flex flex-col border border-neutral-800 rounded-3xl">
+					<p className="text-2xl text-white/30 font-semibold h-[74px] p-[20px]">
+						{params.topicOrUsername}
+					</p>
+					<div className="flex flex-col items-center bg-inherit text-white pb-5 border-b border-neutral-900">
+						<div className="flex align-top w-full max-w-2xl">
+							<div className="w-[130px] h-[130px] bg-[#222222] rounded-md mr-3" />
+							<div className="ml-6 flex-1">
+								<p className="text-[25px] font-semibold mb-3">
+									Username Username
+								</p>
+								<div className="text-white text-[24] font-light mb-1 flex flex-row">
+									<icons.Link />
+									<p className="pl-1">Usernametext</p>
 								</div>
-								<button className="ml-auto px-3 h-[34px] bg-[#222222] text-white/70 hover:opacity-60 rounded-md shadow-inner shadow-outer">
-									Follow
-								</button>
+								<a
+									href="https://"
+									className="text-white/40 text-sm font-light mb-1 flex flex-row items-center"
+								>
+									<icons.Link />
+									<p className="pl-1">usernamelink</p>
+								</a>
 							</div>
+							<button className="ml-auto px-3 h-[34px] bg-[#222222] text-white/70 hover:opacity-60 rounded-md shadow-inner shadow-outer">
+								Follow
+							</button>
 						</div>
-						<div className="flex justify-center mt-10">
-							<div className="flex flex-row gap-20">
-								<ProfileStats number={64} label="Learning" />
-								<ProfileStats number={124} label="To Learn" />
-								<ProfileStats number={12} label="Learned" />
-							</div>
+					</div>
+					<div className="flex justify-center mt-10">
+						<div className="flex flex-row gap-20">
+							<ProfileStats number={64} label="Learning" />
+							<ProfileStats number={124} label="To Learn" />
+							<ProfileStats number={12} label="Learned" />
 						</div>
+					</div>
 
-						<div className="justify-center mt-10 mx-auto w-[50%] space-y-1">
+					{/* <div className="justify-center mt-10 mx-auto w-[50%] space-y-1">
 							<p className="text-white/50 pl-2 pb-3 text-base font-light">
 								Public Pages
 							</p>
 							<ProfilePages topic="Figma" />
 							<ProfilePages topic="React" />
 							<ProfilePages topic="Redux" />
-						</div>
-						<div className="justify-center mt-10 mx-auto w-[50%] space-y-1">
-							<p className="text-white/50 pl-2 pb-3 text-base font-light">
-								Public Links
-							</p>
-							<ProfileLinks
-								linklabel="Figma"
-								link="https://figma.com"
-								topic="Figma"
-							/>
-							<ProfileLinks
-								linklabel="Figma"
-								link="https://figma.com"
-								topic="Figma"
-							/>
-							<ProfileLinks
-								linklabel="Figma"
-								link="https://figma.com"
-								topic="Figma"
-							/>
-						</div>
+						</div> */}
+					<div className="justify-center mt-10 mx-auto w-[50%] space-y-1">
+						<p className="text-white/50 pl-2 pb-3 text-base font-light">
+							Public Links
+						</p>
+						<ProfileLinks
+							linklabel="Figma"
+							link="https://figma.com"
+							topic="Figma"
+						/>
+						<ProfileLinks
+							linklabel="Figma"
+							link="https://figma.com"
+							topic="Figma"
+						/>
+						<ProfileLinks
+							linklabel="Figma"
+							link="https://figma.com"
+							topic="Figma"
+						/>
 					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	)
 }
 
---
+++ web/src/routes/SearchRoute.tsx
@@ -0,0 +1,4 @@
+import { useParams } from "react-router-dom"
+import Sidebar from "../components/Sidebar"
+import { icons } from "../components/Icons"
+import { Link } from "react-router-dom"
